4c46e322a7a1b9ffba4ff8bb6751374e7d057ace
israazwa <160381832+israazwa@users.noreply.github.com>
2025-03-12 01:45:05 +0700

    Add files via upload

---
+++ PythonBasic/PythonBasic/LATIHAN_PYTHON/main.py
@@ -0,0 +1,12 @@
+import latihan_package.alpha as a
+import latihan_package.beta as b
+a.alphaSatu()
+
+import os
+
+os.chdir(r"c:\Users\ASUS\Downloads\PythonBasic\PythonBasic")
+
+import latihan_package.alpha as a, latihan_package.beta as b
+
+a.alphaSatu()
+b.betaSatu()
